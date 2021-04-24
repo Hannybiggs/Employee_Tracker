39f16e49a706cf512b5ae73463916eb64812f7ca
Hanny Biggsmen <hannah.biggam@gmail.com>
2021-04-24 13:07:06 +0200

    updated employee_schema

---
+++ databaseCode.js
@@ -0,0 +1,6 @@
+//Use mySQL, InquirerJS, and console.table NPM packages
+
+const mysql = require('mysql');
+const inquirer = require ('inquirer');
+const cTable = require ('console.table');
+
---
+++ employee_schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS employee_DB;
+
+CREATE DATABASE employee_DB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(30),
+    PRIMARY KEY (id),
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title - VARCHAR(30),
+    salary - DECIMAL (10,2),
+    department_id INT (20),
+    PRIMARY KEY (id),
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR (30),
+    last_name VARCHAR (30),
+    role_id INT (20),
+    manager_id INT (20),
+);
